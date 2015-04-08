ed9052518c958c9b48d1b3e027b742fa515db7f7
Jade King <dev@jadeking.co.uk>
2015-04-08 18:24:30 +0100

    pass filters tests

--- lib/server.rb
+++ lib/server.rb
@@ -1,18 +1,24 @@
 require 'sinatra/base'
 require 'data_mapper'
-require 'tag'
+
 env = ENV['RACK_ENV'] || 'development'
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 require './lib/link'
-
+require './lib/tag'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 # hkjh k
 class BookmarkManager < Sinatra::Base
   get '/' do
     @links = Link.all
+    erb :index
+  end
+
+  get '/tags/:text' do
+    tag = Tag.first(text: params[:text])
+    @links = tag ? tag.links : []
     erb :index
   end
 
