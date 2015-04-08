52e98d143c79454f1648ee2c572ab5d442abd93e
Jade King <dev@jadeking.co.uk>
2015-04-08 16:52:57 +0100

    update to described class

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -1,15 +1,16 @@
+require 'link'
 describe Link do
   context 'demonstration of how data mapper works' do
-    it 'should be created and then retrieved from the DB' do
-      expect(Link.count).to eq(0)
-      Link.create(title: 'Makers Academy',
-                  url: 'http://www.makersacademy.com/')
-      expect(Link.count).to eq(1)
-      link = Link.first
+    it 'be created and then retrieved from the DB' do
+      expect(described_class.count).to eq(0)
+      described_class.create(title: 'Makers Academy',
+                             url: 'http://www.makersacademy.com/')
+      expect(described_class.count).to eq(1)
+      link = described_class.first
       expect(link.url).to eq('http://www.makersacademy.com/')
       expect(link.title).to eq('Makers Academy')
       link.destroy
-      expect(Link.count).to eq(0)
+      expect(described_class.count).to eq(0)
     end
   end
 end
